5279dca44787e7d4c46b5ef2fbaf53fac73f9e9a
Tony MacDonell <tony@teknision.com>
2018-10-17 16:47:23 -0400

    Added ConvoApp so that stub code can be placed in the DialogFlow fulfillment, then all proceeding development happens in your own package.

--- index.js
+++ index.js
@@ -266,4 +266,37 @@ class Convo {
   }
 }
 
-module.exports = {Convo, Say};
+class ConvoApp {
+
+  constructor() {
+    this.registeredIntents = [];
+    this.onRegisterIntents();
+  }
+
+  setClassMappings(obj){
+    Convo.setClassMappings(obj);
+    return this;
+  }
+
+  onRegisterIntents() {}
+
+  registerIntent(intent, intentHandler) {
+    this.registeredIntents.push({intent, intentHandler});
+  }
+
+  bind(dialogFlowApp) {
+    this.registeredIntents.forEach(({intent, intentHandler}) => {
+      dialogFlowApp.intent(intent, (conv, params, extra) => {
+        return intentHandler(new Convo(conv), params, extra);
+      });
+    });
+    return dialogFlowApp;
+  }
+
+  intent(convo, intent, params = {}, option = {}, debugOptions = {}){
+    let registeredIntent = this.registeredIntents.filter(registeredIntent => registeredIntent.intent === intent)[0];
+    return registeredIntent.intentHandler(convo, params, option, debugOptions).then(convo => {return {app:this, convo:convo}});
+  }
+}
+
+module.exports = {Convo, ConvoApp, Say};
--- test.js
+++ test.js
@@ -1,12 +1,42 @@
-const {Convo} = require(`./index`);
+const {Convo, ConvoApp} = require(`./index`);
 
-Convo.ask(
-	new Convo().speak("statement 1")
-	.promise(convo => Promise.resolve("data").then(response =>
-		convo.speak(`statement 2 ${response}`)
-	))
-	.then(convo =>
-		convo.speak("statement 3")
-	),
-	{log:true}
-)
+class MyApplication extends ConvoApp {
+
+    onRegisterIntents() {
+
+      this.registerIntent('welcome', (convo, params, option, debug) => {
+        return Convo.ask(convo.speak("Welcome to my application!"), debug);
+      });
+
+      this.registerIntent('my_fav_color', (convo, params, option, debug) => {
+        return Convo.ask(
+          convo
+            .speak("my favorite color is red.")
+            .speak("What is your favorite color?"),
+          debug
+        );
+      });
+
+      this.registerIntent('your_fav_color', (convo, {color}, option, debug) => {
+        return Convo.ask(
+          convo.speak(`${color} is an amazing choice!`),
+          debug
+        );
+      });
+    }
+}
+
+
+new MyApplication()
+  //User starts your application, and you respond:
+  .intent(new Convo(), 'welcome', null, null, {log:true})
+
+  //User asks for your favorite color:
+  .then(({app, convo}) =>
+    app.intent(new Convo(convo), 'my_fav_color', null, null, {log:true})
+  )
+
+  //User responds with "blue":
+  .then(({app, convo}) =>
+    app.intent(new Convo(convo), 'your_fav_color', {color:"green"}, null, {log:true})
+  );
