8f526cfca7e4a7b4a7237fe4663610cf05bf3313
Bill Neff <BNeff@synacor.com>
2018-10-19 17:52:07 -0400

    cr-tweaks add eslint-config-synacor and fix lint failures fix other minor issues

--- index.js
+++ index.js
@@ -1,302 +1,293 @@
 class Say {
 
-  static append(val) {
-    return new Say().append(val);
-  }
+	static append(val) {
+		return new Say().append(val);
+	}
 
-  static sentence(val) {
-    return new Say().sentence(val);
-  }
-  constructor() {
-    this.str= "";
-  }
+	static sentence(val) {
+		return new Say().sentence(val);
+	}
 
-  append(val) {
-    this.str += val;
-    return this;
-  }
+	constructor() {
+		this.str = '';
+	}
 
-  newline() {
-    return this.append("\n");
-  }
+	append(val) {
+		this.str += val;
+		return this;
+	}
 
-  paragraph() {
-    return this.newline().newline();
-  }
+	newline() {
+		return this.append('\n');
+	}
 
-  sentence(val) {
-    return this.append(" ").append(val);
-  }
+	paragraph() {
+		return this.newline().newline();
+	}
 
-  toString() {
-    return this.str.trim();
-  }
+	sentence(val) {
+		return this.append(' ').append(val);
+	}
+
+	toString() {
+		return this.str.trim();
+	}
+
 }
 
 class Convo {
 
-  static mockConv() {
-    return {
-      surface:{
-        capabilities:{
-          has:() => true
-        }
-      },
-      available:{
-        surfaces:{
-          capabilities:{
-            has:() => true
-          }
-        }
-      },
-      user:{
-        access:{},
-        storage:{}
-      },
-      contexts:{}
-    }
-  }
-  static setClassMappings(mappings) {
-    this._clsMappings = mappings;
-  }
+	static mockConv() {
+		return {
+			surface: {
+				capabilities: {
+					has: () => true
+				}
+			},
+			available: {
+				surfaces: {
+					capabilities: {
+						has: () => true
+					}
+				}
+			},
+			user: {
+				access: {},
+				storage: {}
+			},
+			contexts: {}
+		};
+	}
 
-  static SignIn(obj) {
-    if (this._clsMappings && this._clsMappings['SignIn']) {
-      return this._clsMappings['SignIn'](obj);
-    }
-    return {type:"SignIn", data:obj};
-  }
-  static SimpleResponse(obj) {
-    if (this._clsMappings && this._clsMappings['SimpleResponse']) {
-      return this._clsMappings['SimpleResponse'](obj);
-    }
-    return {type:"SimpleResponse", data:obj};
-  }
-  static NewSurface(obj) {
-    if (this._clsMappings && this._clsMappings['NewSurface']) {
-      return this._clsMappings['NewSurface'](obj);
-    }
-    return {type:"NewSurface", data:obj};
-  }
-  static Image(obj) {
-    if (this._clsMappings && this._clsMappings['Image']) {
-      return this._clsMappings['Image'](obj);
-    }
-    return {type:"Image", data:obj};
-  }
-  static List(obj) {
-    if (this._clsMappings && this._clsMappings['List']) {
-      return this._clsMappings['List'](obj);
-    }
-    return {type:"List", data:obj};
-  }
-  static BasicCard(obj) {
-    if (this._clsMappings && this._clsMappings['BasicCard']) {
-      return this._clsMappings['BasicCard'](obj);
-    }
-    return {type:"BasicCard", data:obj};
-  }
-  static Button(obj) {
-    if (this._clsMappings && this._clsMappings['Button']) {
-      return this._clsMappings['Button'](obj);
-    }
-    return {type:"Button", data:obj};
-  }
+	static setClassMappings(mappings) {
+		this._clsMappings = mappings;
+	}
 
-  static isPromise(obj) {
-    return obj['then'];
-  }
-  static isNotEmpty(str) {
-    return str && str.trim() !== "";
-  }
+	/**
+	 * @private
+	 */
+	static processMethod(type, data) {
+		return this._clsMappings && this._clsMappings[type] ? this._clsMappings[type](data) : { type, data };
+	}
 
-  static complete(convo, action = "close", options = {log:false, logFunc: null}) {
-    if(!convo.conv){
-      if (options.log) {
-          console.log(`${action}:${JSON.stringify(convo, null, 1)}`);
-      }
-      return Promse.resolve(convo);
-    } else {
-      let final_text = convo._write.reduce((acc, text) => acc.sentence(text), new Say()).toString();
-      let final_speech = convo._speak.reduce((acc, text) => acc.sentence(text), new Say()).toString();
-      let promises = [];
-      let textIsNotEmpty = this.isNotEmpty(final_text);
-      let speechIsNotEmpty = this.isNotEmpty(final_speech);
-      let textWasPopulated = textIsNotEmpty || speechIsNotEmpty;
+	/* eslint-disable new-cap */
 
-      let isRunningInDialogFlow = convo.conv[action] !== undefined;
-      let convoFunc = isRunningInDialogFlow ? obj => convo.conv[action](obj) : resp => {
-        if(options.log) {
-          if (options.logFunc) {
-            options.logFunc(action, resp);
-          } else {
-            console.log(`${action}:${JSON.stringify(resp, null, 1)}`);
-          }
-        }
-      };
+	static SignIn(obj) {
+		return this.processMethod('SignIn', obj);
+	}
 
-      if (textWasPopulated) {
-        promises.push(convoFunc(Convo.SimpleResponse({
-          text : textIsNotEmpty ? final_text : final_speech,
-          speech: speechIsNotEmpty ? final_speech : final_text
-        })));
-      }
+	static SimpleResponse(obj) {
+		return this.processMethod('SimpleResponse', obj);
+	}
 
-      convo._rich.forEach((richResponse) => {
-        if (richResponse.media && !richResponse.capabilities && !richResponse.send) {
-          promises.push(convoFunc(richResponse.media));
-          return;
-        }
-        if (convo.conv.surface && convo.conv.surface.capabilities.has(richResponse.capabilities)) {
-          if (!textWasPopulated && richResponse.send && (typeof richResponse.media !== "string")) {
-            promises.push(convoFunc(Convo.SimpleResponse({
-              text : richResponse.send.notification,
-              speech: richResponse.send.notification
-            })));
-          }
-          promises.push(convoFunc(richResponse.media));
-        } else if (richResponse.send && convo.conv.available && convo.conv.available.surfaces.capabilities.has(richResponse.capabilities)) {
-          promises.push(convoFunc(Convo.NewSurface(
-            {
-              context: richResponse.send.context,
-              notification: richResponse.send.notification,
-              capabilities: richResponse.capabilities
-            }
-          )));
-        }
-      });
-      if (isRunningInDialogFlow) {
-        return Promise.all(promises);
-      } else {
-        return Promise.all(promises).then(() => convo);
-      }
+	static NewSurface(obj) {
+		return this.processMethod('NewSurface', obj);
+	}
 
-    }
-  }
-  static prepComplete(obj, action = "close", options) {
-    if (this.isPromise(obj)) {
-      return obj.then(obj => this.prepComplete(obj, action, options));
-    } else {
-      return this.complete(obj, action, options);
-    }
-  }
-  static ask(obj, options) {
-    return this.prepComplete(obj, "ask", options);
-  }
-  static close(obj, options) {
-    return this.prepComplete(obj, "close", options);
-  }
+	static Image(obj) {
+		return this.processMethod('Image', obj);
+	}
 
-  constructor(obj) {
-    if (!obj) {
-      this.conv = Convo.mockConv();
-    } else {
-      this.conv = obj.conv ? obj.conv : obj;
-    }
+	static List(obj) {
+		return this.processMethod('List', obj);
+	}
 
-    this.clear();
-  }
-  clear() {
-    this._write = [];
-    this._speak = [];
-    this._rich = [];
-    return this;
-  }
-  write(message) {
-    this._write.push(message.toString());
-    return this;
-  }
-  speak(message, writeAlso = true) {
-    this._speak.push(message.toString());
-    if (writeAlso) {
-      this.write(message);
-    }
-    return this;
-  }
-  present(media, capabilities = null, send = null) {
-    this._rich.push({capabilities, send, media});
-    return this;
-  }
-  promise(func) {
-    let r = func(this);
-    if (!Convo.isPromise(r)) {
-      return Promise.resolve(r);
-    }
-    return r;
-  }
-  setAccessToken(token) {
-    if (!conv.user) {
-      conv.user = {};
-    }
-    if (!conv.user.access) {
-      conv.user.access = {};
-    }
-    conv.user.access.token = token;
-    return this;
-  }
-  getContext(context) {
-    if (this.conv && this.conv.contexts) {
-      var rContext;
-      if (this.conv.contexts.get) {
-        rContext = this.conv.contexts.get(context);
-      } else {
-        rContext = this.conv.contexts[context];
-      }
-      if (rContext && rContext.parameters) {
-        return rContext.parameters;
-      }
-    }
-    return {};
-  }
-  setContext(context, lifespan, parameters) {
-    if (this.conv && this.conv.contexts) {
-      if (this.conv.contexts.set) {
-        this.conv.contexts.set(context, lifespan, parameters);
-      } else {
-        this.conv.contexts[context] = {lifespan, parameters};
-      }
-    }
-    return this;
-  }
-  getStorage() {
-    if (this.conv && this.conv.user && this.conv.user.storage) {
-      return this.conv.user.storage;
-    }
-    return {};
-  }
+	static BasicCard(obj) {
+		return this.processMethod('BasicCard', obj);
+	}
+
+	static Button(obj) {
+		return this.processMethod('Button', obj);
+	}
+
+	static isNotEmpty(str) {
+		return str && str.trim() !== '';
+	}
+
+	static complete(convo, action = 'close', options = { log: false, logFunc: null }) {
+		if (!convo.conv){
+			if (options.log) {
+				console.log(`${action}:${JSON.stringify(convo, null, 1)}`);
+			}
+			return Promise.resolve(convo);
+		}
+		let finalText = convo._write.reduce((acc, text) => acc.sentence(text), new Say()).toString().trim();
+		let finalSpeech = convo._speak.reduce((acc, text) => acc.sentence(text), new Say()).toString().trim();
+		let promises = [];
+		let textWasPopulated = !!(finalText || finalSpeech);
+
+		let isRunningInDialogFlow = typeof convo.conv[action] === 'function';
+		let convoFunc = isRunningInDialogFlow ? obj => convo.conv[action](obj) : resp => {
+			if (options.log) {
+				if (options.logFunc) {
+					options.logFunc(action, resp);
+				}
+				else {
+					console.log(`${action}:${JSON.stringify(resp, null, 1)}`);
+				}
+			}
+		};
+
+		if (textWasPopulated) {
+			promises.push(convoFunc(Convo.SimpleResponse({
+				text: finalText || finalSpeech,
+				speech: finalSpeech || finalText
+			})));
+		}
+
+		convo._rich.forEach((richResponse) => {
+			if (richResponse.media && !richResponse.capabilities && !richResponse.send) {
+				promises.push(convoFunc(richResponse.media));
+				return;
+			}
+			if (convo.conv.surface && convo.conv.surface.capabilities.has(richResponse.capabilities)) {
+				if (!textWasPopulated && richResponse.send && (typeof richResponse.media !== 'string')) {
+					promises.push(convoFunc(Convo.SimpleResponse({
+						text: richResponse.send.notification,
+						speech: richResponse.send.notification
+					})));
+				}
+				promises.push(convoFunc(richResponse.media));
+			}
+			else if (richResponse.send && convo.conv.available && convo.conv.available.surfaces.capabilities.has(richResponse.capabilities)) {
+				promises.push(convoFunc(Convo.NewSurface(
+					{
+						context: richResponse.send.context,
+						notification: richResponse.send.notification,
+						capabilities: richResponse.capabilities
+					}
+				)));
+			}
+		});
+
+		return isRunningInDialogFlow ? Promise.all(promises) : Promise.all(promises).then(() => convo);
+	}
+
+	/* eslint-enable new-cap */
+	static prepComplete(obj, action = 'close', options) {
+		if (isPromise(obj)) {
+			return obj.then(result => this.prepComplete(result, action, options));
+		}
+
+		return this.complete(obj, action, options);
+	}
+
+	static ask(obj, options) {
+		return this.prepComplete(obj, 'ask', options);
+	}
+
+	static close(obj, options) {
+		return this.prepComplete(obj, 'close', options);
+	}
+
+	constructor(obj) {
+		this.conv = !obj ? Convo.mockConv() : obj.conv || obj;
+		this.clear();
+	}
+
+	clear() {
+		this._write = [];
+		this._speak = [];
+		this._rich = [];
+		return this;
+	}
+
+	write(message) {
+		this._write.push(message.toString());
+		return this;
+	}
+
+	speak(message, writeAlso = true) {
+		this._speak.push(message.toString());
+		if (writeAlso) {
+			this.write(message);
+		}
+		return this;
+	}
+
+	present(media, capabilities = null, send = null) {
+		this._rich.push({ capabilities, send, media });
+		return this;
+	}
+
+	/**
+ * Guaranteen that func returns a promise
+ * @param {Function} func
+ */
+	promise(func) {
+		return Promise.resolve().then(() => func(this) );
+	}
+
+	setAccessToken(token) {
+		if (!this.conv.user) {
+			this.conv.user = {};
+		}
+		if (!this.conv.user.access) {
+			this.conv.user.access = {};
+		}
+		this.conv.user.access.token = token;
+		return this;
+	}
+
+	getContext(context) {
+		let rContext;
+		if (this.conv && this.conv.contexts) {
+			rContext = this.conv.contexts.get ? this.conv.contexts.get(context) : this.conv.contexts[context];
+		}
+		return rContext && rContext.parameters || {};
+	}
+
+	setContext(context, lifespan, parameters) {
+		if (this.conv && this.conv.contexts) {
+			if (this.conv.contexts.set) {
+				this.conv.contexts.set(context, lifespan, parameters);
+			}
+			else {
+				this.conv.contexts[context] = { lifespan, parameters };
+			}
+		}
+		return this;
+	}
+
+	getStorage() {
+		return this.conv && this.conv.user && this.conv.user.storage || {};
+	}
+}
+
+function isPromise(obj) {
+	return !!(obj && obj.then);
 }
 
 class ConvoApp {
 
-  constructor() {
-    this.registeredIntents = [];
-    this.onRegisterIntents();
-  }
+	constructor() {
+		this.registeredIntents = [];
+		this.onRegisterIntents();
+	}
 
-  setClassMappings(obj){
-    Convo.setClassMappings(obj);
-    return this;
-  }
+	setClassMappings(obj){
+		Convo.setClassMappings(obj);
+		return this;
+	}
 
-  onRegisterIntents() {}
+	onRegisterIntents() {}
 
-  registerIntent(intent, intentHandler) {
-    this.registeredIntents.push({intent, intentHandler});
-  }
+	registerIntent(intent, intentHandler) {
+		this.registeredIntents.push({ intent, intentHandler });
+	}
 
-  bind(dialogFlowApp) {
-    this.registeredIntents.forEach(({intent, intentHandler}) => {
-      dialogFlowApp.intent(intent, (conv, params, extra) => {
-        return intentHandler(new Convo(conv), params, extra);
-      });
-    });
-    return dialogFlowApp;
-  }
+	bind(dialogFlowApp) {
+		this.registeredIntents.forEach(({ intent, intentHandler }) => {
+			dialogFlowApp.intent(intent, (conv, params, extra) => intentHandler(new Convo(conv), params, extra));
+		});
+		return dialogFlowApp;
+	}
 
-  intent(convo, intent, params = {}, option = {}, debugOptions = {}){
-    let registeredIntent = this.registeredIntents.filter(registeredIntent => registeredIntent.intent === intent)[0];
-    return registeredIntent.intentHandler(convo, params, option, debugOptions).then(convo => {return {app:this, convo:convo}});
-  }
+	intent(convo, intent, params = {}, option = {}, debugOptions = {}){
+		let registeredIntent = this.registeredIntents.filter(registeredIntent => registeredIntent.intent === intent)[0];
+		return registeredIntent.intentHandler(convo, params, option, debugOptions).then(convo => ({ app: this, convo }));
+	}
 }
 
-module.exports = {Convo, ConvoApp, Say};
+
+module.exports = { Say, Convo, ConvoApp };
--- test.js
+++ test.js
@@ -1,42 +1,36 @@
-const {Convo, ConvoApp} = require(`./index`);
+const { Convo, ConvoApp } = require(`./index`);
 
 class MyApplication extends ConvoApp {
 
-    onRegisterIntents() {
+	onRegisterIntents() {
 
-      this.registerIntent('welcome', (convo, params, option, debug) => {
-        return Convo.ask(convo.speak("Welcome to my application!"), debug);
-      });
+		this.registerIntent('welcome', (convo, params, option, debug) => Convo.ask(convo.speak('Welcome to my application!'), debug));
 
-      this.registerIntent('my_fav_color', (convo, params, option, debug) => {
-        return Convo.ask(
-          convo
-            .speak("my favorite color is red.")
-            .speak("What is your favorite color?"),
-          debug
-        );
-      });
+		this.registerIntent('my_fav_color', (convo, params, option, debug) => Convo.ask(
+			convo
+				.speak('my favorite color is red.')
+				.speak('What is your favorite color?'),
+			debug
+		));
 
-      this.registerIntent('your_fav_color', (convo, {color}, option, debug) => {
-        return Convo.ask(
-          convo.speak(`${color} is an amazing choice!`),
-          debug
-        );
-      });
-    }
+		this.registerIntent('your_fav_color', (convo, { color }, option, debug) => Convo.ask(
+			convo.speak(`${color} is an amazing choice!`),
+			debug
+		));
+	}
 }
 
 
 new MyApplication()
-  //User starts your application, and you respond:
-  .intent(new Convo(), 'welcome', null, null, {log:true})
+//User starts your application, and you respond:
+	.intent(new Convo(), 'welcome', null, null, { log: true })
 
-  //User asks for your favorite color:
-  .then(({app, convo}) =>
-    app.intent(new Convo(convo), 'my_fav_color', null, null, {log:true})
-  )
+//User asks for your favorite color:
+	.then(({ app, convo }) =>
+		app.intent(new Convo(convo), 'my_fav_color', null, null, { log: true })
+	)
 
-  //User responds with "blue":
-  .then(({app, convo}) =>
-    app.intent(new Convo(convo), 'your_fav_color', {color:"green"}, null, {log:true})
-  );
+//User responds with "blue":
+	.then(({ app, convo }) =>
+		app.intent(new Convo(convo), 'your_fav_color', { color: 'green' }, null, { log: true })
+	);
